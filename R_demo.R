0231e098d95373bb04592b11eeb28b84dc8a91ce
Alexandru Mahmoud <amahmou4@jhu.edu>
2020-07-18 16:31:51 -0400

    setwd

--- R_demo.R
+++ R_demo.R
@@ -1,3 +1,4 @@
+setwd("/gvl/projects/current/demo-data")
 data <- read.csv("./rep2_rna_seq_mouse.tsv", header=TRUE, sep="\t",  row.names = 1)
 labels <- read.csv("./datalabels.tsv", header=TRUE, sep="\t",  row.names = 1)
 data_labeled <- rbind(t(labels), data)
